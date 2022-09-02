9617c7090473997f00bdc36a5f45cb55ca2b023d
Kim Beomsu <kimbeomsu@Kimui-MacBookPro.local>
2022-09-03 00:26:46 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+'prettier',
+'plugin:prettier/recommended',
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+      'project' : "./tsconfig.json",
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+       'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/no-var-requires': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    }
+}
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>hello2</div>;
+}
+
+export default App;
